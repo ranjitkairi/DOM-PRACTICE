45e1f1f90cd953e93c6b0b08745db9ea768aa538
ranjit kairi <ranjitkoiri009@gmail.com>
2021-06-08 00:02:01 +0530

    completed DOM-PRACTICE

---
+++ dom.js
@@ -0,0 +1,18 @@
+// getElememnt*
+// const headers = document.getElementsByTagName("h2");
+// const list = document.getElementsByClassName("item");
+// const button = document.getElementById("submit");
+
+// console.log(headers);
+
+// console.log(list);
+
+// console.log(button);
+
+// QuarySelector**
+// const headers = document.querySelector("h2");
+// console.log(headers);
+// const list = document.querySelector("#list a");
+// console.log(list);
+
+// top all like this quary and getElementById******
---
+++ dom2.js
@@ -0,0 +1,35 @@
+         //    *Dom Collection & NodeList*  //
+
+// **
+// const items = document.getElementsByClassName("item");
+
+// console.log(items);
+
+// ***
+// const collectionItems = document.getElementsByClassName("item");
+// const nodeItems = document.querySelectorAll(".item");
+
+// console.log(collectionItems, nodeItems);
+
+// **
+// const collectionItems = document.getElementsByClassName("item");
+// const nodeItems = document.querySelector("#todo-list");
+
+// console.log(nodeItems.childNodes);
+
+// **
+// const collectionItems = document.getElementsByClassName("item");
+// const nodeItems = document.querySelectorAll(".item");
+
+// for (item of collectionItems) {
+//   console.log(item);
+// }
+
+// **
+// const collectionItems = document.getElementsByClassName("item");
+// const nodeItems = document.querySelectorAll(".item");
+
+// nodeItems.forEach(function (item) {
+//   console.log(item);
+// });
+
